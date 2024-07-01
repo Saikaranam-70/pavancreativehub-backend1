ef13dfc0f0de74f8ba870fab9911606e9f508eb5
Saikaranam-70 <saikaranam46@gmail.com>
2024-07-02 01:26:51 +0530

    first commit

--- resetPassword/adminreset.js
+++ resetPassword/adminreset.js
@@ -1,18 +1,37 @@
 const Admin = require('../models/Admin')
 const bcrypt = require('bcryptjs')
+const nodemailer = require('nodemailer')
 
+const transport = nodemailer.createTransport({
+    host:'smtp.gmail.com',
+    port: 587,
+    secure:false,
+    auth:{
+        user:"saimanikantakaranam682@gmail.com",
+        pass:"ffzf jxdr cixz fcxv"
+    }
+})
 const resetPassword = async(req, res)=>{
-    const {email, newPassword} = req.body;
+    const {email} = req.body;
     try {
-        const admin = await Admin.findOne({email});
-        if(!admin){
-            return res.status(400).json({message:"Admin not Found"})
-        }
-        const hashedPassword = await bcrypt.hash(newPassword, 10)
-        admin.password = hashedPassword;
+       const admin = await Admin.findOne({email});
+       const password = admin.password;
 
-        await admin.save();
-        res.status(200).json({success: "Updating password successfully"})
+       const mailOptions ={
+            from: 'saimanikantakaranam682@gmail.com',
+            to:email,
+            subject: 'Pawan creative hub admin pannel password...!!!',
+            text:`Your password is ${password}`
+       }
+       transport.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.error("error sending email", error);
+            return res.status(500).json({error:'Failed to send confirmation email'})
+        }else{
+            console.error("Email sent:", info.response);
+            return res.status(200).json({success:'success to send confirmation email'})
+        }
+    })
     } catch (error) {
         console.log(error);
         return res.status(500).json({error: 'Failed to reset password'})
